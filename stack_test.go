6f5441deff3eb48a18ce0b23eb7c6f1768948500
deepto <ddepp98@outlook.com>
2023-02-18 03:37:22 +0530

    Added TestNotEmpty

--- stack_test.go
+++ stack_test.go
@@ -7,7 +7,20 @@ func TestEmpty(t *testing.T) {
 	stack := NewStack()
 
 	// Assertions to check if stack is empty
+	// Ideology : Empty should be written only after this case is defined
 	if stack.Empty() != true {
 		t.Error("Stack was not empty")
+	}
+}
+
+func TestNotEmpty(t *testing.T) {
+
+	stack := NewStack()
+
+	// Add method is written after this case is defined
+	stack.Add(1)
+
+	if stack.Empty() == true {
+		t.Error("Stack is empty")
 	}
 }
